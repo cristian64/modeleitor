812e50f10008c6d6b4a4ad511151048e80c2f1cd
cristian64 <cristian64@67ee5070-8a92-11de-a64d-234b64dd91b4>
2010-08-29 09:23:27 +0000

    Modificado el menú para que esté en la parte superior sin estar fijado.

--- trunk/menu.php
+++ trunk/menu.php
@@ -1,25 +1,19 @@
 
 				<a class="enlace_menu" href="modelos.php">
 					<span>Modelos</span>
-					<span class="sombra_menu">Modelos</span>
 				</a>
 				<a class="enlace_menu" href="modelo.php">
 					<span>Añadir modelo</span>
-					<span class="sombra_menu">Añadir modelo</span>
 				</a>
 				<a class="enlace_menu" href="fabricantes.php">
 					<span>Fabricantes</span>
-					<span class="sombra_menu">Fabricantes</span>
 				</a>
 				<a class="enlace_menu" href="fabricante.php">
 					<span>Añadir fabricante</span>
-					<span class="sombra_menu">Añadir fabricante</span>
 				</a>
 				<a class="enlace_menu" href="catalogos.php">
 					<span>Catálogos del usuario</span>
-					<span class="sombra_menu">Catálogos del usuario</span>
 				</a>
 				<a class="enlace_menu" href="logout.php">
 					<span>Cerrar sesión de <?php echo $_SESSION["usuario"]; ?></span>
-					<span class="sombra_menu">Cerrar sesión de <?php echo $_SESSION["usuario"]; ?></span>
 				</a>
