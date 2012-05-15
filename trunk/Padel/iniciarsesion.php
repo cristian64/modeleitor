991685f0242bfbad8bdc2e53fddd7262e0195350
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2012-05-15 14:48:26 +0000

    Añadidos un montón de ficheros nuevos y algo del estilo.

---
+++ trunk/Padel/iniciarsesion.php
@@ -0,0 +1,31 @@
+<?php
+	require_once 'minilibreria.php';
+
+	// Procesar parametros e intentar identificar al usuario.
+	$email = $_POST["email"];
+	$contrasena = $_POST["contrasena"];
+    $usuario = ENUsuario::obtenerPorEmail($email);
+	if ($usuario != null)
+	{
+		if ($usuario->getContrasena() == $contrasena)
+		{
+			$_SESSION["usuario"] = serialize($usuario);
+			$_SESSION["fecha"] = date('D/M/Y');
+
+			// Comprobamos si hay que recordar el usuario.
+			if ($_POST["recordar"] == "on")
+			{
+				// Guardamos el usuario y la contraseña en una cookie.
+				setcookie("nombre", $nombre, time() + (30 * 86400));
+				setcookie("contrasena", $contrasena, time() + (30 * 86400));
+				setcookie("fecha", date('D/M/Y'), time() + (30 * 86400));
+			}
+			header("location: index.php");
+			exit();
+		}
+	}
+
+	header("location: index.php?error=Usuario o contraseña incorrecta");
+	exit();
+
+?>
