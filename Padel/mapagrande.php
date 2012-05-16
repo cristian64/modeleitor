0a40a4a697e6c1fe5b4cb6cc4191cc2eed0e0293
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2012-05-16 14:21:23 +0000

    Boton lila.

--- Padel/mapagrande.php
+++ Padel/mapagrande.php
@@ -4,11 +4,12 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Padel en Matola - Mapa</title>
+        <link type="text/css" rel="stylesheet" href="estilo/freshbutton.css" media="screen" title="Estilo principal" />
 		<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
 		<script src="javascript/mapa.js" type="text/javascript"></script>
 	</head>
 	
 	<body onload="GoogleMaps3(true);" style="margin: 0; padding: 0;">
-		<div id="mapaapi" style="width: 100%; height: 100%;"></div>
+		<div id="mapaapi"></div>
 	</body>
 </html>
