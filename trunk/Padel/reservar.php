04a8774bc34a9a0a8092cc3d81a2bf3494ad0309
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2012-05-18 01:44:59 +0000

    Calculados algunos valores para las fechas en php de las reservas.php

--- trunk/Padel/reservar.php
+++ trunk/Padel/reservar.php
@@ -1,16 +1,51 @@
 <?php
 include_once "base.php";
 baseSuperior("Reservar pista");
+
+// Obtenemos el día, el día máximo, y las variables para ir avanzando en el bucle.
+$now = new DateTime();
+$now->setTime(0, 0, 0);
+$dia = new DateTime();
+if (isset($_GET["dia"]))
+{
+    $dia = DateTime::createFromFormat('d/m/Y', $_GET["dia"]);
+    if ($dia == false)
+        $dia = new DateTime();
+}
+$dia->setTime(0, 0, 0);
+$intervalo = $dia->diff($dia);
+$intervalo->d = 15; //VALOR DESDE minilibreria y tal, y poner tambien aqui keys y otras cosillas
+$maximodia = new DateTime();
+$maximodia->add($intervalo);
+$maximodia->setTime(0, 0, 0);
+if ($dia > $maximodia)
+    $dia = $maximodia;
+if ($dia < $now)
+    $dia = $now;
+$tiempoInicial = clone $dia;
+$tiempoFinal = clone $dia;
+$tiempoInicial->setTime(8, 0);
+$tiempoFinal->setTime(23, 59, 59);
+$intervalo = $tiempoInicial->diff($tiempoInicial);
+$intervalo->i = 30;
+
+// Obtenemos las reservas de las 6 pistas para el día elegido.
+/*$reservas1 = ENReserva::obtenerTodosPorPista(1, $dia);
+$reservas2 = ENReserva::obtenerTodosPorPista(2, $dia);
+$reservas3 = ENReserva::obtenerTodosPorPista(3, $dia);
+$reservas4 = ENReserva::obtenerTodosPorPista(4, $dia);
+$reservas5 = ENReserva::obtenerTodosPorPista(5, $dia);*/
+
 ?>
                     <div id="reservar">
                         <h3><span>Reservar pista</span></h3>
                         <div id="resumenreserva">
                             <div id="datepicker"></div>
                             <form action="operarregistro.php" method="post" enctype="multipart/form-data" onsubmit="return validarReserva(this);">
-                                <div><label>Día: </label><input type="text" value="11/02/2012" name="dia" style="width: 100px;" /></div>
-                                <div><label>Pista: </label><input type="text" value="3" name="pista" style="width: 30px;" /></div>
-                                <div><label>Desde las </label><input type="text" value="10:30" name="desde" style="width: 50px;" /><label> hasta las </label><input type="text" value="12:30" name="hasta" style="width: 50px;" /></div>
-                                <div><label>Duración: </label><input type="text" value="60" name="duracion" style="width: 30px;" /><label> minutos</label></div>
+                                <div><label>Día: </label><input type="text" value="<?php echo $dia->format('d/m/Y'); ?>" name="dia" style="width: 100px;" /></div>
+                                <div><label>Pista: </label><input type="text" value="" name="pista" style="width: 30px;" /></div>
+                                <div><label>Desde las </label><input type="text" value="" name="desde" style="width: 50px;" /><label> hasta las </label><input type="text" value="" name="hasta" style="width: 50px;" /></div>
+                                <div><label>Duración: </label><input type="text" value="" name="duracion" style="width: 30px;" /><label> minutos</label></div>
                                 <div><input type="submit" value="Confirmar reserva" name="" class="freshbutton-big" /></div>
                             </form>
                             <div id="tiempo">
@@ -30,14 +65,6 @@ baseSuperior("Reservar pista");
                                     <td>Pista 6</td>
                                 </tr>
 <?php
-$tiempoInicial = new DateTime();
-$tiempoFinal = new DateTime();
-$tiempoInicial->setTime(8, 0);
-$tiempoFinal->setTime(23, 59, 59);
-
-$intervalo = $tiempoInicial->diff($tiempoInicial);
-$intervalo->i = 30;
-
 while ($tiempoInicial < $tiempoFinal)
 {
 ?>
@@ -81,7 +108,8 @@ while ($tiempoInicial < $tiempoFinal)
                     
                     $(function() {
                         $( "#datepicker" ).datepicker({
-                            minDate: 0, maxDate: "+7D"
+                            minDate: 0, maxDate: '<?php echo $maximodia->format('d/m/Y'); ?>',
+                            defaultDate: '<?php echo $dia->format('d/m/Y'); ?>'
                         });
                     });
                     </script>
