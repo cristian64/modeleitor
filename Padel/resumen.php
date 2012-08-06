f23efc2906c68e1c3c1b080b62eed9c7bc28099b
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2012-08-06 21:49:07 +0000

    Added icons at the upper part. Resumen ready to add the php code.

--- Padel/resumen.php
+++ Padel/resumen.php
@@ -20,6 +20,16 @@ baseSuperior("Resumen del sistema");
 ?>
         <div id="resumen">
             <h3><span>Resumen del sistema</span></h3>
+            <div id="lineasresumen">
+                <div><span class="descripcion">Reservas totales: </span><span class="dato">103</span></div>
+                <div><span class="descripcion">Reservas para hoy: </span><span class="dato">6</span></div>
+                <div><span class="descripcion">Reservas durante los últimos 7 días: </span><span class="dato">59</span></div>
+                <div><span class="descripcion">Tiempo total de reservas: </span><span class="dato">581230 minutos</span></div>
+                <div><span class="descripcion">Tiempo medio por reserva: </span><span class="dato">90 minutos</span></div>
+                <div><span class="descripcion">Usuarios registrados: </span><span class="dato">507</span></div>
+                <div><span class="descripcion">Usuarios registrados durante los últimos 7 días: </span><span class="dato">21</span></div>
+            </div>
+            
         </div>
 <?php
 baseInferior();
