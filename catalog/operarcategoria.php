e3b9229ddf82815ac40417f5e1d247e2240b56ce
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2012-08-22 21:19:27 +0000

    Añadidos los mensajes. Ya se pueden crear y borrar categorías.

---
+++ catalog/operarcategoria.php
@@ -0,0 +1,138 @@
+<?php
+include_once "minilibreria.php";
+
+/*$usuario = getUsuario();
+if ($usuario == null)
+{
+    $_SESSION["mensaje_aviso"] = "Tu sesión ha caducado. Debes iniciar sesión antes de poder ver los datos.";
+    header("location: iniciarsesion.php");
+    exit();
+}
+
+if (!$usuario->getAdmin())
+{
+    $_SESSION["mensaje_aviso"] = "Área restringida a administradores.";
+    header("location: index.php");
+    exit();
+}*/
+
+$operacion = getPost("op");
+switch ($operacion)
+{
+    case "eliminar":
+        eliminar();
+        break;
+    case "anadir":
+        anadir();
+        break;
+    case "editar":
+        editar();
+        break;
+}
+
+function eliminar()
+{
+    $id = getPost("id");
+    if ($id != "")
+    {
+        $categoria = ENCategoria::getById($id);
+        if ($categoria != null)
+        {
+            if ($categoria->delete())
+            {
+                $_SESSION["mensaje_exito"] = "La categoría $id ha sido eliminada correctamente";
+                header("location: categorias");
+                exit();
+            }
+        }
+    }
+    
+    $_SESSION["mensaje_error"] = "No se pudo eliminar la categoría $id";
+    header("location: categorias");
+    exit();
+}
+
+function editar()
+{
+    $id = getPost("id");
+    
+    $categoria = ENCategoria::getById($id);
+    if ($categoria != null)
+    {
+        $categoria->setNombre(getPost("nombre"));
+        $categoria->setDescripcion(getPost("descripcion"));
+        $categoria->setZindex(getPost("zindex"));
+        $categoria->setMostrar(getPost("mostrar") == "yes");
+        if ($categoria->update())
+        {
+            $_SESSION["mensaje_exito"] = "La categoría $id ha sido guardada correctamente";
+            header("location: categorias");
+            exit();
+        }
+        else
+        {
+            $_SESSION["mensaje_error"] = "No se pudo guardar la categoría $id";
+            header("location: categorias");
+            exit();
+        }        
+    }
+    else
+    {
+        $_SESSION["mensaje_error"] = "No se pudo encontrar la categoría $id";
+        header("location: categorias");
+        exit();
+    }
+}
+
+function anadir()
+{
+    $id_padre = getPost("id_padre");
+    if (intval($id_padre) == 0 || ENCategoria::getById($id_padre) != null)
+    {
+        $categoria = new ENCategoria();
+        $categoria->setIdPadre($id_padre);
+        $categoria->setNombre(getPost("nombre"));
+        $categoria->setDescripcion(getPost("descripcion"));
+        $categoria->setZindex(getPost("zindex"));
+        $categoria->setMostrar(getPost("mostrar") == "yes");
+        if ($categoria->save())
+        {
+            $_SESSION["mensaje_exito"] = "La categoría ha sido añadida correctamente";
+            header("location: categorias");
+            exit();
+        }
+        else
+        {
+            $_SESSION["mensaje_error"] = "No se pudo añadir la categoría";
+            header("location: categorias");
+            exit();
+        }        
+    }
+    else
+    {
+        $_SESSION["mensaje_error"] = "No se pudo encontrar la categoría padre $id_padre";
+        header("location: categorias");
+        exit();
+    }
+}
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
