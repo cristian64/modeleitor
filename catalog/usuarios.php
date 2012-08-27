21e6262935e25fc1dc0f8853ba49e082460a0306
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2012-08-27 23:24:00 +0000

    Lista de usuarios básica.

---
+++ catalog/usuarios.php
@@ -0,0 +1,52 @@
+<?php
+
+include_once "base.php";
+
+$usuario = getUsuario();
+if ($usuario == null)
+{
+    $_SESSION["mensaje_aviso"] = "Tu sesión ha caducado. Debes iniciar sesión antes de poder ver los datos.";
+    header("location: index.php");
+    exit();
+}
+
+if (!$usuario->getAdmin())
+{
+    $_SESSION["mensaje_aviso"] = "Área restringida a administradores.";
+    header("location: index.php");
+    exit();
+}
+
+baseSuperior("Usuarios");
+
+?>
+<h3>Usuarios</h3>
+<div>
+    <table>
+        <tr class="cabecera">
+            <td>ID</td>
+            <td class="estirar">Nombre</td>
+            <td>Email</td>
+            <td>Teléfono</td>
+            <td>Activo</td>
+            <td></td>
+        </tr>
+<?php
+    $usuarios = ENUsuario::get();
+    foreach ($usuarios as $i)
+    {
+        echo "<tr class=\"fila\">";
+        echo "<td class=\"centrada\">".rellenar($i->getId(), '0', 6)."</td>";
+        echo "<td>".$i->getNombre()."</td>";
+        echo "<td class=\"centrada\">".$i->getEmail()."</td>";
+        echo "<td class=\"centrada\">".$i->getTelefono()."</td>";
+        echo "<td class=\"centrada\">".($i->getActivo() == 1 ? "sí" : "no")."</td>";
+        echo "<td></td>";
+        echo "</tr>";
+    }
+?>
+    </table>
+
+</div>
+        
+<?php baseInferior(); ?>
