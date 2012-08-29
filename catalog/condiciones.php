8319d539bea9c547eb8c86ddbcc97f3d33fffab4
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2012-08-29 07:46:44 +0000

    Añadidas privacidad y condiciones.

---
+++ catalog/condiciones.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once "base.php";
+
+baseSuperior("Privacidad y datos");
+?>
+
+<div class="texto-privacidad">
+    <h3>CONDICIONES DE USO</h3>
+    <div>
+        <strong>1. General</strong>
+        Utilizar esta página web y/o registrarse como usuario presupone la aceptación de estas condiciones.
+    </div>
+    <div>
+        <strong>2. Propiedad de esta página web</strong>
+        Esta página web es propiedad de Herederos de José Aguilera Moreno S.L.U. en adelante Calzados JAM. Todos los derechos de su contenido, de las imágenes, textos, diseño y software son propiedad de Calzados JAM.
+        Todos los elementos de esta página, incluyendo sin limitaciones, su diseño y su contenido están protegidos por las leyes de la Propiedad Intelectual, Propiedad Industrial y tratados internacionales referidos a los Derecho de Autor.
+    </div>
+    <div>
+        <strong>3. Uso de su Contenido</strong>
+        A menos que sea explícitamente autorizado por Calzados JAM, usted no puede reproducir, transmitir o explotar el contenido de esta web de ningún modo.
+    </div>
+    <div>
+        <strong>4. Responsabilidades</strong>
+        <ul>
+            <li>A pesar de que Calzados JAM pone el máximo esfuerzo en garantizar la precisión y exactitud en el contenido de esta página web, ésta puede contener errores o inexactitudes. Consecuentemente no garantizamos la veracidad, la fiabilidad o la exactitud de su contenido.</li>
+            <li>Calzados JAM rechaza la responsabilidad sobre la información contenida en páginas web de terceras partes conectadas por enlaces (links), con las páginas web de Calzados JAM.</li>
+            <li>Bajo ninguna circunstancia, Calzados JAM será tenido como responsable de ningún daño o perjuicio de ningún tipo surgido de, o en relación, al uso de esta página web.</li>
+            <li>Deberá indemnizar a Calzados JAM contra cualquier daño o perjuicio que resulte del incumplimiento por su parte de las presentes condiciones o del uso del contenido de esta web sin previa autorización.</li>
+        </ul>
+        </div>
+    <div>
+        <strong>5. Divisibilidad</strong>
+        Si alguna estipulación de las presentes condiciones carece o pasase a carecer de validez o resultara no ejecutable según el derecho aplicable, dicha estipulación no tendría efecto, pero solamente en la medida de esa precisa falta de validez, y no afectará a ninguna otra estipulación de las presentes condiciones.
+    </div>
+    <div>
+        <strong>6. Legislación y jurisdicción aplicable</strong>
+        Estas condiciones de uso deben ser interpretadas y están regidas por la legislación española vigente. Cualquier disputa derivada de estas condiciones deberá dirimirse ante los tribunales de Elche, España. No obstante, esto no impedirá a Calzados JAM ejercer el derecho a resolver cualquier litigio en otra jurisdicción competente.
+    </div>
+    <div>
+        <strong>7. Modificaciones</strong>
+        Calzados JAM se reserva el derecho a cambiar el contenido de esta página web, en cualquier momento y sin aviso previo.
+    </div>
+</div>
+        
+<?php baseInferior(); ?>
