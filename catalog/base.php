aee7d7c4ece2ef9db4b4f1fbd36fa0a1f166265d
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2012-08-30 09:34:16 +0000

    Búsqueda básica que busca lo escrito en alguno de los campos.

--- catalog/base.php
+++ catalog/base.php
@@ -54,7 +54,7 @@ function baseSuperior($titulo)
                             </td>
                             <td class="estirar"></td>
                             <td id="busqueda">
-                                <form method="GET" action="buscar"><input type="text" value="" placeholder="nº de referencia, descripción" <?php if (!esMovil()) echo "class=\"animacion\""; ?> /></form>
+                                <form method="GET" action="catalogo"><input type="text" value="" name="busqueda" placeholder="nº de referencia, descripción" <?php if (!esMovil()) echo "class=\"animacion\""; ?> /></form>
                             </td>
                             <td>
                                 <?php
