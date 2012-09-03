59bf7b078494d40c5a0daad83d75f7c14fd595ef
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2012-09-03 00:10:29 +0000

    Eliminado $EMAIL_CONTACTO. Añadido Google Analitycs.cvnbc

--- trunk/catalog/base.php
+++ trunk/catalog/base.php
@@ -41,6 +41,21 @@ function baseSuperior($titulo)
         <script src="js/formularios.js" type="text/javascript"></script>
         <script src="js/cookies.js" type="text/javascript"></script>
         
+        <script type="text/javascript">
+
+         var _gaq = _gaq || [];
+         _gaq.push(['_setAccount', 'UA-8410217-1']);
+         _gaq.push(['_trackPageview']);
+
+         (function() {
+            var ga = document.createElement('
+        script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+         })();
+
+        </script>
+        
         <!--<meta name="google-translate-customization" content="83036bbb495e9259-b92db425e1be685c-g15db1c5c55c82e4b-12" />-->
     </head>
     <body>
