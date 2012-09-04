152b0c2b85e30492678f3f476e28057f726616ba
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2012-09-04 11:38:12 +0000

    No se hace submit a no ser que los campos este completos.

--- trunk/catalog/js/formularios.js
+++ trunk/catalog/js/formularios.js
@@ -23,41 +23,10 @@ function validarIdentificacion(formulario)
         return false;
 }
 
-function validarRegistro(formulario)
+function validarRegistro()
 {
-    var alerta = "";
-
-    // Comprobamos que la contraseña es válida.
-    if (formulario.nombre.value.length < kMinNombre || formulario.nombre.value.length > kMaxNombre)
-        alerta = alerta + "- El nombre debe tener entre " + kMinNombre + " y " + kMaxNombre + " caracteres.\n\n";
-
-    // Comprobamos que la contraseña es válida.
-    if (formulario.contrasena.value.length < kMinContrasena || formulario.contrasena.value.length > kMaxContrasena)
-        alerta = alerta + "- La contraseña debe tener entre " + kMinContrasena + " y " + kMaxContrasena + " caracteres.\n\n";
-
-    // Comprobamos que las contraseñas coinciden.
-    if (formulario.contrasena.value != formulario.contrasena2.value)
-        alerta = alerta + "- Las contraseñas no coinciden.\n\n";
-
-    // Comprobamos que se ha marcado algún sexo.
-    if (!formulario.sexo[0].checked && !formulario.sexo[1].checked)
-        alerta = alerta + "- Es necesario seleccionar algún sexo.\n\n";
-
-    // Comprobamos que la dirección de correo electrónico es correcta.
-    var expReg = new RegExp("^[a-zA-Z]+([\\.-]?[a-zA-Z0-9]+)*@[a-zA-Z]+([\\.-]?[a-zA-Z0-9]+)*(\\.[a-zA-Z0-9]{2,4})+$");
-    if (!expReg.test(formulario.email.value))
-        alerta = alerta + "- La dirección de e-mail no tiene un formato válido.\n\n";
-
-    // Si se ha concatenado algún error, se muestra el mensaje y se aborta el "submit" del formulario devolviendo falso.
-    if (alerta != "")
-    {
-        alert(alerta);
-        return false;
-    }
-    else
-    {
-        return true;
-    }
+    comprobarEmail(); comprobarContrasena(); comprobarContrasena2(); comprobarNombre(); comprobarTelefono();
+    return comprobarEmail() && comprobarContrasena() && comprobarContrasena2() && comprobarNombre() && comprobarTelefono();
 }
 
 function comprobarEmail()
@@ -67,10 +36,12 @@ function comprobarEmail()
     if (!expReg.test(formulario.email.value))
     {
         $('#error-email').show('blind');
+        return false;
     }
     else
     {
         $('#error-email').hide();
+        return true;
     }
 }
 
@@ -80,10 +51,12 @@ function comprobarContrasena()
     if (formulario.contrasena.value.length < kMinContrasena || formulario.contrasena.value.length > kMaxContrasena)
     {
         $('#error-contrasena').show('blind');
+        return false;
     }
     else
     {
         $('#error-contrasena').hide();
+        return true;
     }
 }
 
@@ -93,10 +66,12 @@ function comprobarContrasena2()
     if (formulario.contrasena.value != formulario.contrasena2.value)
     {
         $('#error-contrasena2').show('blind');
+        return false;
     }
     else
     {
         $('#error-contrasena2').hide();
+        return true;
     }
 }
 
@@ -106,10 +81,27 @@ function comprobarNombre()
     if (formulario.nombre.value.length < kMinNombre || formulario.nombre.value.length > kMaxNombre)
     {
         $('#error-nombre').show('blind');
+        return false;
     }
     else
     {
         $('#error-nombre').hide();
+        return true;
+    }
+}
+
+function comprobarTelefono()
+{
+    var formulario = document.getElementById('crearcuenta');
+    if (formulario.telefono.value.length == 0)
+    {
+        $('#error-telefono').show('blind');
+        return false;
+    }
+    else
+    {
+        $('#error-telefono').hide();
+        return true;
     }
 }
 
