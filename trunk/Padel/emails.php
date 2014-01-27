69420d6371119b7e076c19d987927f5a3f734c6d
cristian64@gmail.com <cristian64@gmail.com@67ee5070-8a92-11de-a64d-234b64dd91b4>
2014-01-27 21:40:37 +0000

    Nuevo número de teléfono y eliminado Fran de los emails.

--- trunk/Padel/emails.php
+++ trunk/Padel/emails.php
@@ -4,6 +4,5 @@ $EMAILS_RESERVAS = array();
 $EMAILS_RESERVAS[0] = "beatriz@autofima.com";
 $EMAILS_RESERVAS[1] = "Santiago@autofima.com";
 $EMAILS_RESERVAS[2] = "emihyundai@hotmail.com";
-$EMAILS_RESERVAS[3] = "fran@padelelche.com";
 
 ?>
